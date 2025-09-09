95ee1b0d24b1d03a75acc6267dd57eef4964d0b4
vixsonous <cjvicro@gmail.com>
2025-09-10 01:30:40 +0800

    Added dynamic metadata for blog list Added structured data for blog list

--- web/src/app/(public)/columns/list/[pageNo]/page.tsx
+++ web/src/app/(public)/columns/list/[pageNo]/page.tsx
@@ -17,11 +17,36 @@ type Props = {
 export async function generateMetadata({params,searchParams }: Props): Promise<Metadata> {
   const {pageNo} = await params;
   const {category} = await searchParams;
+  const response = await ServerApiService.get("/get-blogs?page_no=" + pageNo + "&category=" + category);
+  const blogsResponse = await ServerApiResponseService.getResponseData<z.infer<typeof GetBlogSchema.GetBlogList>>(response);
 
-  console.log(`Page No: ${pageNo} ${category}`);
-
+  const title = `${category || ""}ブログ${pageNo}`;
+  const description = `わんごはん公式ブログ`;
   return {
-    title: 'Columns ' + pageNo + (category ? ' ' + category : '')
+    title: title,
+    keywords: ["ブログ",
+      "わんごはん",
+      "犬用手作りごはん",
+      "ペットレシピサイト"].concat(blogsResponse.blogs.map( blog => blog.title)),
+    creator: "わんごはん公式",
+    description: description,
+    openGraph: {
+      title: title,
+      description: description,
+      url: 'https://wangohanjp.com', // Your website URL
+      type: "article",
+      images: [
+        { url: blogsResponse.blogs[0].blog_image.startsWith("r2://") ?
+            process.env.BASE_PUBLIC_BUCKET_URL + blogsResponse.blogs[0].blog_image.split("r2://")[1] :
+            blogsResponse.blogs[0].blog_image, width: 500, height: 500, alt: blogsResponse.blogs[0].title }
+      ]
+    },
+    robots: {
+      index:true,
+      follow: true,
+      nocache: false,
+    },
+
   }
 }
 
@@ -39,7 +64,25 @@ export default async function Columns({params, searchParams}: Props) {
 
   const blogsResponse = await ServerApiResponseService.getResponseData<z.infer<typeof GetBlogSchema.GetBlogList>>(response);
 
-  return (
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    "name": `${category || ""}ブログ${pageNo}`,
+    "description": "わんごはん公式ブログ",
+    "image": blogsResponse.blogs[0].blog_image.startsWith("r2://") ?
+      process.env.BASE_PUBLIC_BUCKET_URL + blogsResponse.blogs[0].blog_image.split("r2://")[1] :
+      blogsResponse.blogs[0].blog_image,
+    "author": {
+      "@type": "Person",
+      "name": "わんごはん公式"
+    },
+    "keywords": ["ブログ",
+      "わんごはん",
+      "犬用手作りごはん",
+      "ペットレシピサイト"].concat(blogsResponse.blogs.map( blog => blog.title)),
+  };
+
+  return <>
     <section>
       <div className="flex flex-col items-center relative mt-16">
         <h1 className="w-full bottom-16 p-4 bg-white border-2 rounded-2xl border-primary-text max-w-max absolute text-2xl whitespace-pre-wrap lg:text-4xl font-bold text-primary-text">
@@ -57,7 +100,7 @@ export default async function Columns({params, searchParams}: Props) {
         {["全て", "レシピ特集", "基礎知識", "その他"].map((item, idx) => (
           <li key={idx}>
             <Link href={`/columns/list/${pageNo}?category=${item}`}>
-              <Button className={"bg-secondary-bg rounded-full text-primary-text border-2 border-primary-text hover:bg-secondary-bg/50"}>
+              <Button className={"bg-secondary-bg text-xs md:text-sm rounded-full text-primary-text border-2 border-primary-text hover:bg-secondary-bg/50"}>
                 <Image preload={true} width={16} height={16} src={"/icons/column/paw2.png"} alt={"category icon"}/>
                 <p>{item}</p>
               </Button>
@@ -71,7 +114,7 @@ export default async function Columns({params, searchParams}: Props) {
             return (
               <Link
                 key={idx}
-                href={`/columns/` + blog.blog_id}
+                href={`/columns/show/${blog.blog_id}/${blog.title}`}
                 className="col-span-1 flex flex-col items-center justify-center"
               >
                 <BlogItem blog={blog} />
@@ -84,5 +127,9 @@ export default async function Columns({params, searchParams}: Props) {
       </div>
       <PaginationWithLinks totalCount={blogsResponse.total_blogs} pageSize={6} page={Number(pageNo)} />
     </section>
-  )
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+    />
+  </>
 }
