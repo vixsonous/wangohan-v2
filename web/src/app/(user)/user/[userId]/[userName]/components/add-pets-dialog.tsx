af03ef47c5097afb617bed32ed1c3890b92e6565
vixsonous <cjvicro@gmail.com>
2025-09-19 21:53:49 +0800

    Fix the button trigger not working if register_pet is true

--- web/src/app/(user)/user/[userId]/[userName]/components/add-pets-dialog.tsx
+++ web/src/app/(user)/user/[userId]/[userName]/components/add-pets-dialog.tsx
@@ -7,27 +7,30 @@ import {
   DialogTitle,
   DialogTrigger
 } from "@/components/ui/dialog";
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useState} from "react";
 import dynamic from "next/dynamic";
 import {QueryClientProvider} from "@tanstack/react-query";
 import {queryClient} from "@/lib/tanstack-query";
 import {useRouter, useSearchParams} from "next/navigation";
+import Button from "@/components/Button";
 const AddPetForm = dynamic(() => import("./add-pets-form"), {ssr: false, loading: () => <span>Loading</span>})
 
 export default function AddPetsDialog({user_id}: {user_id: number}) {
   const searchParams = useSearchParams();
-  const triggerRef = useRef<HTMLButtonElement>(null);
+  const [open, setOpen] = useState(false);
   const router = useRouter();
   useEffect(() => {
-    if(searchParams.get("register_pet") && triggerRef.current) {
-      triggerRef.current.click();
+    if(searchParams.get("register_pet")) {
+      setOpen(true);
       router.replace(window.location.pathname);
     }
   }, [searchParams]);
   return (
-    <Dialog>
-      <DialogTrigger ref={triggerRef}>
-        <span className="text-lg font-bold cursor-pointer hover:brightness-50 transition-all">愛犬を登録する</span>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild={true}>
+        <Button type={"button"}>
+          <span className="text-lg font-bold cursor-pointer hover:brightness-50 transition-all">愛犬を登録する</span>
+        </Button>
       </DialogTrigger>
       <DialogContent draggable={true} className={"sm:max-w-2xl"}>
         <DialogHeader>
