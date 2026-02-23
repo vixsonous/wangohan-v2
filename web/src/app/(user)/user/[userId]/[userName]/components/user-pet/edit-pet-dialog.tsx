e498d571f77302c88de8077a8d602e839141448e
Victor Chiong <cjvicro@gmail.com>
2026-02-24 02:20:27 +0800

    Add set pets

--- web/src/app/(user)/user/[userId]/[userName]/components/user-pet/edit-pet-dialog.tsx
+++ web/src/app/(user)/user/[userId]/[userName]/components/user-pet/edit-pet-dialog.tsx
@@ -1,4 +1,3 @@
-"use client";
 import {
   Dialog,
   DialogContent,
@@ -7,17 +6,17 @@ import {
   DialogTitle,
   DialogTrigger
 } from "@/components/ui/dialog";
-import React, { useState} from "react";
+import React, {Dispatch, SetStateAction, useState} from "react";
 import dynamic from "next/dynamic";
 import {QueryClientProvider} from "@tanstack/react-query";
 import {queryClient} from "@/lib/tanstack-query";
 import Button from "@/components/Button";
 import {ScrollArea} from "@/components/ui/scroll-area";
-import {PetProps} from "@/app/(user)/user/[userId]/[userName]/components/user-pets-carousel";
+import {PetProps} from "@/app/(user)/user/[userId]/[userName]/components/user-pet/user-pets-carousel";
 import Loader from "@/components/loader";
 const PetForm = dynamic(() => import("../pets-form"), {ssr: false, loading: () => <Loader />})
 
-export default function EditPetDialog({user_id, pet}: {user_id: number, pet: PetProps}) {
+export default function EditPetDialog({user_id, pet, setPets}: {user_id: number, pet: PetProps, setPets: Dispatch<SetStateAction<Array<PetProps>>>}) {
   const [open, setOpen] = useState(false);
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -26,7 +25,7 @@ export default function EditPetDialog({user_id, pet}: {user_id: number, pet: Pet
           <span className="text-lg font-bold cursor-pointer hover:brightness-50 transition-all">愛犬を登録する</span>
         </Button>
       </DialogTrigger>
-      <DialogContent draggable={true} className={"sm:max-w-2xl p-0"}>
+      <DialogContent className={"sm:max-w-2xl p-0"}>
         <DialogHeader className={"pt-4"}>
           <DialogTitle className={"text-center"}>
             ペットを登録しましょう！
@@ -37,7 +36,7 @@ export default function EditPetDialog({user_id, pet}: {user_id: number, pet: Pet
         </DialogHeader>
         <ScrollArea className={"max-h-128 px-4 pb-4"}>
           <QueryClientProvider client={queryClient}>
-            <PetForm user_id={user_id} pet={pet}/>
+            <PetForm user_id={user_id} pet={pet} setOpen={setOpen} setPets={setPets}/>
           </QueryClientProvider>
         </ScrollArea>
       </DialogContent>
