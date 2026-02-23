27eaf5e205829b1543b47baccc1db00d5db5ca27
Victor Chiong <cjvicro@gmail.com>
2026-02-24 02:19:54 +0800

    Moved, add client pets

--- web/src/app/(user)/user/[userId]/[userName]/components/user-pet/add-pets-dialog.tsx
+++ web/src/app/(user)/user/[userId]/[userName]/components/user-pet/add-pets-dialog.tsx
@@ -1,4 +1,3 @@
-"use client";
 import {
   Dialog,
   DialogContent,
@@ -7,18 +6,18 @@ import {
   DialogTitle,
   DialogTrigger
 } from "@/components/ui/dialog";
-import React, {useEffect, useState} from "react";
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
 import dynamic from "next/dynamic";
 import {QueryClientProvider} from "@tanstack/react-query";
 import {queryClient} from "@/lib/tanstack-query";
 import {useRouter, useSearchParams} from "next/navigation";
 import Button from "@/components/Button";
 import {ScrollArea} from "@/components/ui/scroll-area";
-import {PetProps} from "@/app/(user)/user/[userId]/[userName]/components/user-pets-carousel";
+import {PetProps} from "@/app/(user)/user/[userId]/[userName]/components/user-pet/user-pets-carousel";
 import Loader from "@/components/loader";
-const AddPetForm = dynamic(() => import("./pets-form"), {ssr: false, loading: () => <Loader />})
+const AddPetForm = dynamic(() => import("../pets-form"), {ssr: false, loading: () => <Loader />})
 
-export default function AddPetsDialog({user_id, pet}: {user_id: number, pet?: PetProps | undefined}) {
+export default function AddPetsDialog({user_id, pet, setPets}: {user_id: number, pet?: PetProps | undefined, setPets: Dispatch<SetStateAction<Array<PetProps>>>}) {
   const searchParams = useSearchParams();
   const [open, setOpen] = useState(false);
   const router = useRouter();
@@ -47,7 +46,7 @@ export default function AddPetsDialog({user_id, pet}: {user_id: number, pet?: Pe
         </DialogHeader>
         <ScrollArea className={"max-h-128 px-4 pb-4"}>
           <QueryClientProvider client={queryClient}>
-            <AddPetForm user_id={user_id} pet={pet}/>
+            <AddPetForm user_id={user_id} pet={pet} setOpen={setOpen} setPets={setPets}/>
           </QueryClientProvider>
         </ScrollArea>
       </DialogContent>
