1c149315d6e4d624385369b0f69160d09a632ccb
Victor Chiong <cjvicro@gmail.com>
2026-02-24 12:23:09 +0800

    Add update pet route

--- backend/server/Pet/pet-routes.ts
+++ backend/server/Pet/pet-routes.ts
@@ -7,4 +7,5 @@ const upload = multer({dest: 'uploads/', storage: multer.memoryStorage()});
 export const petRouter = Router();
 
 petRouter.post("/", upload.single('pet_image'), PetController.postPet);
+petRouter.put("/:pet_id", upload.single('pet_image'), PetController.putPet);
 petRouter.get("/birthday", PetController.getBirthdayMonthPets);
