7610e30add78d3fa99f19b31158191fd1fc9a326
Victor Chiong <cjvicro@gmail.com>
2026-02-24 14:42:10 +0800

    Add database error class

--- backend/server/errors/error-types.ts
+++ backend/server/errors/error-types.ts
@@ -4,3 +4,10 @@ export class ImageServiceError extends Error {
     this.name = "ImageServiceError";
   }
 }
+
+export class DatabaseError extends Error {
+  constructor(message: string, cause?: unknown) {
+    super(message);
+    this.name = "DatabaseError";
+  }
+}
